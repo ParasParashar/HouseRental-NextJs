89351b9d9e144bfe8b3b0a7a3ca9a2965f57558f
prsprshr@gmail.com <123573626+ParasParashar@users.noreply.github.com>
2023-08-27 00:32:07 +0530

    after update2 commit

--- app/components/Calender.tsx
+++ app/components/Calender.tsx
@@ -1,26 +1,37 @@
-"use client"
-import {Range,RangeKeyDict,DateRange} from "react-date-range"
-import 'react-date-range/dist/styles.css'
-import 'react-date-range/dist/theme/default.css'
-interface CalenderProps{
-    value:Range;
-    onChange:(value:RangeKeyDict)=>void;
-    disabledDates?:Date[]
-}
-const Calender:React.FC<CalenderProps> = ({value,onChange,disabledDates}) => {
-  return (
-   <DateRange
-   rangeColors={['#262626']}
-   ranges={[value]}
-   date={new Date()}
-   onChange={onChange}
-   direction='vertical'
-   showDateDisplay ={false}
-   minDate={new Date()}
-   disabledDates={disabledDates}
-   />
+'use client';
 
-  )
+import { 
+  DateRange, 
+  Range, 
+  RangeKeyDict
+} from 'react-date-range';
+
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+
+interface DatePickerProps {
+  value: Range,
+  onChange: (value: RangeKeyDict) => void;
+  disabledDates?: Date[];
 }
 
-export default Calender
+const DatePicker: React.FC<DatePickerProps> = ({
+  value,
+  onChange,
+  disabledDates
+}) => {
+  return ( 
+    <DateRange
+      rangeColors={['#262626']}
+      ranges={[value]}
+      date={new Date()}
+      onChange={onChange}
+      direction="vertical"
+      showDateDisplay={false}
+      minDate={new Date()}
+      disabledDates={disabledDates}
+    />
+   );
+}
+ 
+export default DatePicker;
